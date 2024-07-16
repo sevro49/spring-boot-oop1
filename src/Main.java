234d2a42970511828e3676eb5f68f82eeac1c8cb
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-07-16 21:06:48 +0300

    created Product class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product1 = new Product();
+        product1.setName("Kahve makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setUnitInStock(3);
+        product1.setImageUrl("image1.jpg");
+
+        Product product2 = new Product();
+        product2.setName("phillips Kahve makinesi");
+        product2.setDiscount(5);
+        product2.setUnitPrice(6500);
+        product2.setUnitInStock(3);
+        product2.setImageUrl("image2.jpg");
+
+        Product product3 = new Product();
+        product3.setName("xyz  makinesi");
+        product3.setDiscount(8);
+        product3.setUnitPrice(7900);
+        product3.setUnitInStock(8);
+        product3.setImageUrl("image3.jpg");
+
+        Product[] products = {product1, product2, product3};
+
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+
     }
 }
